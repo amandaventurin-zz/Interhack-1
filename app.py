6e193e5170bae58b67c5e1a3383b43ce8d0600e6
André Felipe J. Nastari <andfenastari@gmail.com>
2019-08-24 16:54:56 -0300

    Andre

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, escape, request, redirect, render_template
+import os
+import psycopg2
+from flask import Flask, escape, request, redirect, render_template, g
 
 app = Flask(__name__)
+
+database_url = os.environ.get('DATABASE_URL')
+
+def get_db():
+  if 'db' not in g:
+    g.db = psycopg2.connect(database_url)
+  
+  return g.db
+
+@app.teardown_appcontext
+def teardown_db():
+  db = g.pop('db', None)
+
+  if db is not None:
+    db.close()
 
 @app.route('/')
 def hello():
